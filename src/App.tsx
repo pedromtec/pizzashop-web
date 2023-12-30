52e7c002b3cd219f17971187c16accf4eb3d5cc1
Pedro Soares <pedromtec@gmail.com>
2023-12-30 16:49:31 -0300

    feat: Add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { Button } from '@/components/ui/button'
 import './global.css'
 
 export function App() {
-  
   return (
-   <div className='bg-slate-100 flex justify-center items-center h-screen'>
-      <div className='flex justify-center items-center w-full max-w-lg h-80 bg-slate-600 rounded-sm'>
+    <div className="justify-center bg-slate-100 flex items-center h-screen">
+      <div className="flex justify-center items-center w-full max-w-lg h-80 bg-slate-600 rounded-sm">
         <Button>Clique aqui</Button>
       </div>
-   </div>
+    </div>
   )
 }
-
