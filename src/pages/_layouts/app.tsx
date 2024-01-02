d884520d70c8bb5fcfb431fd3c205a41db310bc1
Pedro Soares <pedromtec@gmail.com>
2024-01-02 14:12:50 -0300

    feat: Create sign-in page

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 export function AppLayout() {
   return (
-    <div>
+    <div className="grid min-h-screen grid-cols-2">
       <h1>App layout</h1>
       <div>
         <Outlet />
